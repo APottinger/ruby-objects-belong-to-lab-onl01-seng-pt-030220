39a4092fcc6f0f1adfa4ec009696472ab406bc2d
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-25 18:21:10 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+  attr_accessor :name, :album, :genre 
+  
+  def initialize(name, album, genre)
+    @name = name 
+    @album = album
+    @genre = genre 
+  end
+end
+
+mr_beast = Artist.new("Mr. Beast", "Hiiits", "Lo-Fi")
+
+mr_beast.album
