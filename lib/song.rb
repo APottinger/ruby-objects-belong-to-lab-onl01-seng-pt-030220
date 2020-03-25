f624171793329de1fd94b701bd21fe304b81c468
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-25 18:18:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,13 @@
 class Song 
-  attr_accessor :name, :genre #macro for calling these methods on instances of song
+  attr_accessor :name, :artist :genre #macro for calling these methods on instances of song
   
-  def initialize(name, genre) #upon initialization, songs have a name and genre and artist(not mentioned here for the Artist class
+  def initialize(name, artist genre) #upon initialization, songs have a name and genre and artist
     @name = name 
+    @artist = artist 
     @genre = genre
   end
 end
 
-playtime = Song.new("Playtime", "Lo-Fi")
+playtime = Song.new("Playtime", "The Beast", "Lo-Fi")
 
   
