a28b2a9ac9a723d9feba83896fb4797590452b97
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-25 18:43:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,3 @@
-class Artist
-  attr_accessor :name, :genre
- 
-  def initialize(name, genre) 
-    @name = name
-    @genre = genre
-  end
- 
+class Artist 
+  attr_accessor :name 
 end
- 
-drake = Artist.new("Drake", "rap")
-hotline_bling = Song.new("Hotline Bling")
- 
-hotline_bling.artist = drake
